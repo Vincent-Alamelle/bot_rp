844054d3505842284413b4a5cefe61fa566a14ef
vincent-alamelle <vincent.alamelle@etu.univ-amu.fr>
2020-10-21 16:16:18 +0200

    Initial commit - Basics

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client;
+
+bot.on("message", function (message) {
+
+});
+
+bot.login('NzU5ODAxMzE4MDA0MzU5MjE4.X3CyHg.S6QHN9m2ld5X8txj9sHnSEDgnVc');
